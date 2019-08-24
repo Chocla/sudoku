9df554eab0291d9024bdd6459c03dfe68ca469f1
Chocla <19178866+Chocla@users.noreply.github.com>
2019-08-24 11:06:06 -0700

    Replace arrays with vectors

--- generator.cpp
+++ generator.cpp
@@ -6,23 +6,23 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-//implement for 9x9, then 16x16
 
+//implement for 9x9, then 16x16
 const int N = 9;
 const int NR = 3; //Square root of N
 //for debugging
-void printBoard(int board[N][N]) {
+void printBoard(std::vector<std::vector<int>> board) {
     std::cout << std::endl;
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            std::cout << board[i][j] << " ";
+    for(std::vector<int> row : board) {
+        for(int i: row) {
+            std::cout << i << " ";
         }
-        std::cout << "\n";
+        std::cout << std::endl;
     }
-    std::cout << "\n";
+    std::cout << std::endl;
 }
 //checks if candidate value is unique to col,row, and box
-bool isValid(int candidate,int cell, int board[N][N]) {
+bool isValid(int candidate,int cell, std::vector<std::vector<int>> board) {
     int row = cell / N;
     int col = cell % N;
     for(int i = 0; i < N; i++) {
@@ -48,19 +48,17 @@ bool isValid(int candidate,int cell, int board[N][N]) {
 bool isUnique(int board[N][N]) {
 
 }
-bool solveBoard(int board[N][N], int excludeCell = N*N, int exlcludeVal = -1) {
-    bool flags[N*N]; //keep track of initial board values
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            if(board[i][j] != 0) {
-                flags[N*i + j] = true;
-            } else {
-                flags[N*i + j] = false;
-            }
-        }
+bool solveBoard(std::vector<std::vector<int>> board) {
+    printBoard(board);
+    std::vector<bool> flags(N*N);
+    std::vector<std::vector<int>> boardCopy = board;
+    for(int i = 0; i < N*N; i++) {
+        if(board[i / N][i % N] != 0) {
+            flags[i] = true;
+        } 
     }
-    // std::cout << "Flags Made" << std::endl;
     int currentCell = 0;
+    int maxCell = 0;
     while(currentCell < N*N) {
         if(flags[currentCell]) {
             currentCell++;
@@ -69,73 +67,65 @@ bool solveBoard(int board[N][N], int excludeCell = N*N, int exlcludeVal = -1) {
         int row = currentCell / N;
         int col = currentCell % N;
         bool backtrack = true;
-        //check each number 1-9,starting at current value+1
-        for(int i = board[row][col] + 1;i <= N ; i++) { //[1,N]
-            //if we find one that is valid, continue to the next cell
-            int candidate = std::rand() % N + 1;
-            if(isValid(candidate, currentCell,board) && 
-                !(currentCell == excludeCell && candidate == exlcludeVal)) {
-                board[row][col] = candidate;
+        for(int i = boardCopy[row][col] + 1; i <= N; i++) {
+            // int candidate = std::rand() % N + 1;
+            if(isValid(i, currentCell,boardCopy)) {
+                boardCopy[row][col] = i;
                 backtrack = false;
                 break;
             }
-        } 
-        //zero out cell
-        //decrement currentCell until flags[currentCell] is false
-        // std::cout << "Current Cell:" << currentCell << std::endl;
+        }
         if(backtrack) {
-            board[row][col] = 0;
+            boardCopy[row][col] = 0;
             currentCell--;
             while(flags[currentCell]) {
                 currentCell--;
             }
-            if(currentCell < 0) {
+            if (currentCell < 0) {
                 return false;
             }
             continue;
         }
         currentCell++;
     }
-    // std::cout << "Board Solved" << std::endl;
-    //board solved
+    printBoard(boardCopy);
     return true;
 }
-
 //TODO: fix non unique solutions problem
-void removeCells(int board[N][N],int numCells) {
-    int puzzleBoard[N][N];
-    std::copy(&board[0][0],&board[0][0]+(N*N), &puzzleBoard[0][0]);
-    std::vector<int> changedCells(numCells);
-    std::vector<int> changedVals(numCells);
+// void removeCells(int board[N][N],int numCells) {
+//     int puzzleBoard[N][N];
+//     std::copy(&board[0][0],&board[0][0]+(N*N), &puzzleBoard[0][0]);
+//     std::vector<int> changedCells(numCells);
+//     std::vector<int> changedVals(numCells);
 
-    int count = 0;
-    while(count < numCells) {
-        if(count % 5 == 0) {
-            std::cout << "Removed " << count << " Cells" << std::endl;
-        }
-        int candidatecell = std::rand() % (N*N);
-        int row = candidatecell / N;
-        int col = candidatecell % N;
-        int tmp = board[row][col];
-        if(tmp == 0) {
-            continue; //repeated cell
-        }
-        board[row][col] = 0;
-        if (!solveBoard(board,candidatecell,tmp)) {
-            count++;
-            changedCells[count] = candidatecell;
-            changedVals[count] = tmp;
-        } else {
-            board[row][col] = tmp;
-        }
-        for(int i : changedCells) {
-                board[i / N][i % N] = 0;
-        }
-    }
-}
+//     int count = 0;
+//     while(count < numCells) {
+//         if(count % 5 == 0) {
+//             std::cout << "Removed " << count << " Cells" << std::endl;
+//         }
+//         int candidatecell = std::rand() % (N*N);
+//         int row = candidatecell / N;
+//         int col = candidatecell % N;
+//         int tmp = board[row][col];
+//         if(tmp == 0) {
+//             continue; //repeated cell
+//         }
+//         board[row][col] = 0;
+//         if (!solveBoard(board,candidatecell,tmp)) {
+//             count++;
+//             changedCells[count] = candidatecell;
+//             changedVals[count] = tmp;
+//         } else {
+//             board[row][col] = tmp;
+//         }
+//         for(int i : changedCells) {
+//                 board[i / N][i % N] = 0;
+//         }
+//     }
+// }
 
 // "\setrow{ROWNUM}{ {1}, {2},.....{9}}"
-std::vector<std::string> formatBoard(int board[N][N]) {
+std::vector<std::string> formatBoard(std::vector<std::vector<int>> board) {
     std::vector<std::string> rowStrings(N);
 
     for(int i = 0; i < N; i++) {
@@ -150,7 +140,6 @@ std::vector<std::string> formatBoard(int board[N][N]) {
         }
         line.pop_back();
         line += "}";
-        // std::cout << line << std::endl;
         rowStrings[i] = line;
     }
 
@@ -164,7 +153,7 @@ void createTexFile() {
         outfile << line << std::endl;
         if (line.compare("    \\begin{lpsudoku}") == 0) {
             //GENERATE PUZZLE
-            int board[N][N] = {
+            std::vector<std::vector<int>> board = {
                 {5,3,0, 0,7,0, 0,0,0},
                 {6,0,0, 1,9,5, 0,0,0},
                 {0,9,8, 0,0,0, 0,6,0},
@@ -190,25 +179,10 @@ void createTexFile() {
     return;
 }
 
-int countZeros(int board[N][N]) {
-    int count = 0;
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N;j++) {
-            if(board[i][j] == 0 ){
-                count++;
-            }
-        }
-    }
-    return count;
-}
-
 int main() {
     std::srand(unsigned(std::time(0)));
-    int blank[N][N];
-    for(int i = 0; i < N; i++)
-        for(int j = 0; j < N; j++)
-            blank[i][j] = 0;
-    int board[N][N] = {
+    std::vector<std::vector<int>> blank(N,std::vector<int>(N));
+    std::vector<std::vector<int>> board = {
         {5,3,0, 0,7,0, 0,0,0},
         {6,0,0, 1,9,5, 0,0,0},
         {0,9,8, 0,0,0, 0,6,0},
@@ -221,7 +195,7 @@ int main() {
         {0,0,0, 4,1,9, 0,0,5},
         {0,0,0, 0,8,0, 0,7,9}
     };
-    int solved[N][N] = {
+    std::vector<std::vector<int>> solved = {
         {5,3,4, 6,7,8, 9,1,2},
         {6,7,2, 1,9,5, 3,4,8},
         {1,9,8, 3,4,2, 5,6,7},
@@ -234,13 +208,16 @@ int main() {
         {2,8,7, 4,1,9, 6,3,5},
         {3,4,5, 2,8,6, 1,7,9}
     };
+    solveBoard(board);
+    // std::vector<std::string> rows = formatBoard(board);
+
+    // printBoard(board);
+    // printBoard(solved);
 
     // solveBoard(blank);
     // printBoard(blank);
     // removeCells(blank,40);
     // printBoard(blank);
-    // int count = countZeros(blank);
-    // std::cout << "Actual Zeros: " << count << std::endl; 
 
-    createTexFile();
+    // createTexFile();
 }
